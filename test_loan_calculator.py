f7e93d32f4d94bb6de247be23b68b5d86b42043b
Ozan Ozgur <ozan1.ozgur@gmail.com>
2017-10-18 19:45:48 +0100

    added csv data validation tests.

--- test_loan_calculator.py
+++ test_loan_calculator.py
@@ -9,19 +9,36 @@ from loan_calculator import SimpleLoanCalculator
 class TestSimpleLoanCalculator(unittest.TestCase):
 
     def test_file_exists(self):
-        pass
+        loan_calc_obj = SimpleLoanCalculator('./test_data/test_data_1.csv', 200)
+        self.assertTrue(loan_calc_obj._check_file_exists())
 
     def test_file_not_found(self):
-        pass
+        loan_calc_obj = SimpleLoanCalculator('./test_data/foo_bar.csv', 200)
+        with self.assertRaises(FileNotFoundError):
+            loan_calc_obj._check_file_exists()
 
     def test_csv_reader_object(self):
-        pass
+        loan_calc_obj = SimpleLoanCalculator('./test_data/test_data_1.csv', 200)
+        reader = loan_calc_obj._read_csv_file()
+        data_rows = sum(1 for row in reader)
+        self.assertEqual(data_rows, 7)
+        loan_calc_obj.file_handle.close()
 
     def test_csv_reader_object_with_no_data_rows(self):
-        pass
+        loan_calc_obj = SimpleLoanCalculator('./test_data/test_data_2.csv', 200)
+        try:
+            with self.assertRaises(ValueError):
+                loan_calc_obj._read_csv_file()
+        finally:
+            loan_calc_obj.file_handle.close()
 
     def test_unexpected_number_of_columns_in_csv_file(self):
-        pass
+        loan_calc_obj = SimpleLoanCalculator('./test_data/test_data_3.csv', 200)
+        try:
+            with self.assertRaises(ValueError):
+                loan_calc_obj._read_csv_file()
+        finally:
+            loan_calc_obj.file_handle.close()
 
 
 if __name__ == '__main__':
