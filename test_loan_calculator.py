d0de6baac45ffde8d0d5d12808fa7877639429f3
Ozan Ozgur <ozan1.ozgur@gmail.com>
2017-10-18 19:27:51 +0100

    added test stubs

---
+++ test_loan_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from loan_calculator import SimpleLoanCalculator
+
+
+class TestSimpleLoanCalculator(unittest.TestCase):
+
+    def test_file_exists(self):
+        pass
+
+    def test_file_not_found(self):
+        pass
+
+    def test_csv_reader_object(self):
+        pass
+
+    def test_csv_reader_object_with_no_data_rows(self):
+        pass
+
+    def test_unexpected_number_of_columns_in_csv_file(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
