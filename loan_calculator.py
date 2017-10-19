f91fc3acf30b50a3b3a2500d713e042daf2e3f52
Ozan Ozgur <ozan1.ozgur@gmail.com>
2017-10-19 02:52:05 +0100

    final tweaks to module and tests.

--- loan_calculator.py
+++ loan_calculator.py
@@ -2,11 +2,14 @@
 
 import os
 import csv
+from decimal import Decimal
 
 
 class SimpleLoanCalculator(object):
+
     MIN_LOAN = 1000
     MAX_LOAN = 15000
+    LOAN_BORROW_LENGTH_YEARS = 3
 
     def __init__(self, data_file, requested_loan):
         self.market_data_file = data_file
@@ -15,27 +18,27 @@ class SimpleLoanCalculator(object):
 
     def _is_requested_loan_amount_valid(self):
         """ Checks requested loan amount meets upper and lower limits of
-        the loan system and is in increments of £100.
+        the loan system and is in increments of 100.
         """
-        try:
-            self.requested_loan_amount = int(self.requested_loan_amount)
-        except ValueError:
-            return False
-
-        if SimpleLoanCalculator.MIN_LOAN <= self.requested_loan_amount \
-                <= SimpleLoanCalculator.MAX_LOAN \
+        if (self.requested_loan_amount >= SimpleLoanCalculator.MIN_LOAN) \
+                and (self.requested_loan_amount <= SimpleLoanCalculator.MAX_LOAN) \
                 and self.requested_loan_amount % 100 == 0:
             return True
         else:
-            return False
+            raise ValueError(
+                '>> Error: please request an amount between £{:,} and £{:,} '
+                'in increments of 100!'.format(
+                    SimpleLoanCalculator.MIN_LOAN, SimpleLoanCalculator.MAX_LOAN))
 
     def _check_file_exists(self):
+        """ Checks if the data file passed as an argument to the class instance
+        during instantiation actually exists, otherwise bail out of the program.
+        """
         if os.path.isfile(os.path.abspath(self.market_data_file)):
             return True
         else:
-            print('>> Error: file "{}" not found!'.format(self.market_data_file))
-            # then bail out
-            raise FileNotFoundError
+            raise FileNotFoundError('>> Error: file "{}" not found!'.format(
+                self.market_data_file))
 
     def _read_csv_file(self):
         """ Reads and returns a reader object which will iterate over lines in
@@ -47,6 +50,7 @@ class SimpleLoanCalculator(object):
         Manually close the file handle
         """
         self._check_file_exists()
+        self._is_requested_loan_amount_valid()
         self.file_handle = open(self.market_data_file)
         reader = csv.DictReader(self.file_handle)
         self._verify_number_of_columns_in_csv_file(reader)
@@ -58,23 +62,70 @@ class SimpleLoanCalculator(object):
             self.file_handle.seek(0)
             reader = csv.DictReader(self.file_handle)
         except StopIteration:
-            print('>> Error: file "{}" does not contain data!'.format(
-                self.market_data_file))
-            raise ValueError
+            raise ValueError(
+                '>> Error: file "{}" does not contain data!'.format(
+                    self.market_data_file))
         else:
             return reader
 
     def _verify_number_of_columns_in_csv_file(self, csv_reader_obj):
+        """ In the market lender data file, we expect 3 columns of data.
+        """
         expected_columns = 3
         if len(csv_reader_obj.fieldnames) == expected_columns:
-            return
+            return True
         else:
-            print('>> Error: expected {} columns, but found {}!'.format(
-                expected_columns, len(csv_reader_obj.fieldnames)))
-            raise ValueError
+            raise ValueError(
+                '>> Error: expected {} columns, but found {}!'.format(
+                    expected_columns, len(csv_reader_obj.fieldnames)))
 
-    def _find_lowest_rate_lender(self):
+    def get_lowest_suitable_rate_from_lenders(self):
         reader = self._read_csv_file()
-        sorted_list = sorted(
-            reader, key=lambda row: int(row['Rate']), reverse=True)
 
+        # sort lenders by interest rate, converting it from string to decimal
+        # and ordering it by lowest to highest
+        sorted_list = sorted(
+            reader, key=lambda row: Decimal(row['Rate']), reverse=False)
+        self.file_handle.close()
+
+        for lender in sorted_list:
+            # requested loan is an int, because we are dealing with increments
+            # of 100 and assume that the lender amount could be fractional,
+            # therefore convert that to a decimal
+            if self.requested_loan_amount <= Decimal(lender['Available']):
+                return lender['Rate']
+            else:
+                continue
+        return None
+
+    def calculate_quote_using_monthly_compounding(self, interest_rate):
+        """ This method calculates the new future amount based on
+        monthly compounding interest.
+
+        Returns:
+            - the total future amount due
+            - percentage annual interest
+            - monthly repayment amount
+
+        Using the formula :
+
+        A = P (1 + r/n) ^ nt
+
+        Where:
+
+        A = the future amount, including interest
+        P = the principal amount (the initial loan amount)
+        r = the annual interest rate (decimal)
+        n = the number of times that interest is compounded per year
+        t = the number of years the money is borrowed for
+        """
+        P = Decimal(self.requested_loan_amount)
+        r = Decimal(interest_rate)
+        n = Decimal(12)
+        t = Decimal(SimpleLoanCalculator.LOAN_BORROW_LENGTH_YEARS)
+
+        A = P * (1 + (r / n)) ** (n * t)
+        monthly_repayments = \
+            A / (SimpleLoanCalculator.LOAN_BORROW_LENGTH_YEARS * 12)
+
+        return round(A, 2), round(r * 100, 1), round(monthly_repayments, 2)
--- test_loan_calculator.py
+++ test_loan_calculator.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from decimal import Decimal
 
 from loan_calculator import SimpleLoanCalculator
 
 
-class TestSimpleLoanCalculator(unittest.TestCase):
+class TestLoanCalculatorDataValidation(unittest.TestCase):
     
     def setUp(self):
         self.loan_min = 1000
-        self.load_max = 15000
+        self.loan_max = 15000
 
     def test_file_exists(self):
-        lc_obj = SimpleLoanCalculator('./test_data/test_data_1.csv', self.loan_min)
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_1.csv', self.loan_min)
         self.assertTrue(lc_obj._check_file_exists())
 
     def test_file_not_found(self):
@@ -21,15 +23,17 @@ class TestSimpleLoanCalculator(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             lc_obj._check_file_exists()
 
-    def test_csv_reader_object(self):
-        lc_obj = SimpleLoanCalculator('./test_data/test_data_1.csv', self.loan_min)
+    def test_csv_reader_object_has_data(self):
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_1.csv', self.loan_min)
         reader = lc_obj._read_csv_file()
         data_rows = sum(1 for row in reader)
         self.assertEqual(data_rows, 7)
         lc_obj.file_handle.close()
 
     def test_csv_reader_object_with_no_data_rows(self):
-        lc_obj = SimpleLoanCalculator('./test_data/test_data_2.csv', self.loan_min)
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_2.csv', self.loan_min)
         try:
             with self.assertRaises(ValueError):
                 lc_obj._read_csv_file()
@@ -37,7 +41,8 @@ class TestSimpleLoanCalculator(unittest.TestCase):
             lc_obj.file_handle.close()
 
     def test_unexpected_number_of_columns_in_csv_file(self):
-        lc_obj = SimpleLoanCalculator('./test_data/test_data_3.csv', self.loan_min)
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_3.csv', self.loan_min)
         try:
             with self.assertRaises(ValueError):
                 lc_obj._read_csv_file()
@@ -45,24 +50,50 @@ class TestSimpleLoanCalculator(unittest.TestCase):
             lc_obj.file_handle.close()
 
     def test_is_requested_loan_amount_valid(self):
-        lc_obj = SimpleLoanCalculator('./test_data/test_data_1.csv', self.loan_min)
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_1.csv', self.loan_min)
         self.assertTrue(lc_obj._is_requested_loan_amount_valid())
 
-    def test_is_requested_loan_amount_valid_if_string(self):
-        lc_obj = SimpleLoanCalculator('./test_data/test_data_1.csv', '1000')
-        self.assertTrue(lc_obj._is_requested_loan_amount_valid())
+    def test_requested_loan_amount_below_minimum_limit(self):
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_1.csv', self.loan_min-1)
+        with self.assertRaises(ValueError):
+            lc_obj._is_requested_loan_amount_valid()
 
-    def test_is_requested_loan_amount_valid_if_nonconvertable_string(self):
-        lc_obj = SimpleLoanCalculator('./test_data/test_data_1.csv', 'fubar')
-        self.assertFalse(lc_obj._is_requested_loan_amount_valid())
+    def test_requested_loan_amount_above_max_limit(self):
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_1.csv', self.loan_max+1)
+        with self.assertRaises(ValueError):
+            lc_obj._is_requested_loan_amount_valid()
 
-    def test_is_requested_loan_amount_below_minimum_limit(self):
-        lc_obj = SimpleLoanCalculator('./test_data/test_data_1.csv', self.loan_min-1)
-        self.assertFalse(lc_obj._is_requested_loan_amount_valid())
+    def test_if_requested_loan_amount_not_in_100_increments(self):
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_1.csv', self.loan_max+40)
+        with self.assertRaises(ValueError):
+            lc_obj._is_requested_loan_amount_valid()
 
-    def test_is_requested_loan_amount_above_max_limit(self):
-        lc_obj = SimpleLoanCalculator('./test_data/test_data_1.csv', self.load_max+1)
-        self.assertFalse(lc_obj._is_requested_loan_amount_valid())
+
+class TestLoanCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.loan_min = 1000
+        self.loan_max = 15000
+
+    def test_get_lowest_suitable_interest_rate_from_lenders(self):
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_1.csv', self.loan_min)
+        interest_rate = lc_obj.get_lowest_suitable_rate_from_lenders()
+        self.assertEqual(interest_rate, '0.069')
+
+    def test_calculate_quote_using_monthly_compounding(self):
+        lc_obj = SimpleLoanCalculator(
+            './test_data/test_data_1.csv', 5000)
+        interest_rate = '0.05'
+        principle_amount, interest_rate_percentage, repayments = \
+            lc_obj.calculate_quote_using_monthly_compounding(interest_rate)
+        self.assertEqual(principle_amount, Decimal('5807.36'))
+        self.assertEqual(interest_rate_percentage, Decimal('5.0'))
+        self.assertEqual(repayments, Decimal('161.32'))
 
 
 if __name__ == '__main__':
